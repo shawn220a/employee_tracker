d771ac712c144e8f14979b0dc1c1f1a2f0ced6e8
shawn220a <shawn.tschoepe@icloud.com>
2020-03-05 17:44:00 -0600

    Finish creating connection to database

--- app.js
+++ app.js
@@ -9,15 +9,18 @@ let connection = mysql.createConnection({
   port: 3306,
   user: 'root',
   password: 'shawn220321',
-  database: 'my_db'
-})
+  database: 'employee_db'
+});
 
-connection.connect()
+connection.connect((err) => {
+  if (err) throw err;
+  start();
+});
 
 connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
   if (err) throw err
 
   console.log('The solution is: ', rows[0].solution)
-})
+});
 
-connection.end()
+connection.end();
